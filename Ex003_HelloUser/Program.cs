a4ef22dcb6a62b3cade2d4c9e6a957ba37d85660
tatianakub <pershik86@mail.ru>
2022-10-13 14:47:26 +0300

    Commit 3

--- Ex003_HelloUser/Program.cs
+++ Ex003_HelloUser/Program.cs
@@ -1,3 +1,9 @@
-﻿string username = Console.ReadLine();
-Console.WriteLine ("Привет, ");
-Console.WriteLine ("username");
+﻿//Console.WriteLine("Введите ваше имя ");
+// string username = Console.ReadLine();
+// Console.WriteLine("Привет, ");
+// Console.WriteLine(username);
+
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
