05407b14c0207afd81050587da2604b0f68273a1
tatianakub <pershik86@mail.ru>
2022-10-16 22:27:31 +0300

    add Example006

---
+++ Example006_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Hello Masha");
+}
+else
+{
+    Console.Write("Hello ");
+    Console.WriteLine("Username");
+}
