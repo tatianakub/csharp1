b227a9b9e2ef20913456d356c2cb7b2ab1eb9b6a
tatianakub <pershik86@mail.ru>
2022-10-17 14:29:40 +0300

    Add Task002

--- Example006_if_else/Program.cs
+++ Example006_if_else/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите имя пользователя: ");
 
-string username = Console.ReadLine();
+string username = Console.ReadLine() ;
 
 if(username == "Маша")
 {
--- Example010_IntroArray/Program.cs
+++ Example010_IntroArray/Program.cs
@@ -4,25 +4,14 @@
     if(arg2 > result) result = arg2;
     if(arg3 > result) result = arg3;
     return result; 
-}
-int a1 = 15;
-int b1 = 21;
-int c1 = 39;
-int a2 = 12;
-int b2 = 23;
-int c2 = 3355;
-int a3 = 13;
-int b3 = 23;
-int c3 = 33555;
+} //           0   1  2  3 ...         8
+int[] array = {11,21,31,41,15,61,17,18,19}; //массив 
+array[0] = 12;
+Console.WriteLine(array[4]);
 
-//int max1 = Max(a1, b1, c1);
-//int max2 = Max(a2, b2, c2);
-//int max3 = Max(a3, b3, c3);
-//int max = Max(max1, max2, max3);
+//int max = Max (
+   // Max(a1, b1, c1),
+   // Max(a2, b2, c2),
+    //Max(a3, b3, c3));
 
-int max = Max (
-    Max(a1, b1, c1),
-    Max(a2, b2, c2),
-    Max(a3, b3, c3));
-
-Console.WriteLine(max);
+//Console.WriteLine(max);
