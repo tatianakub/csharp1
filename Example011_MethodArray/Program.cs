b5a61c6f0de3160ca6a1487b447b1175eed48ce2
tatianakub <pershik86@mail.ru>
2022-10-17 20:58:48 +0300

    Add Example011

---
+++ Example011_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {1, 2, 3, 4, 5, 6, 7, 8 }; // массив здесь такой
+
+int n = array.Length; 
+int find = 2;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
