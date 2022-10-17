8103ec83ae2d5231fb5a3aeae54f572108cbd37a
tatianakub <pershik86@mail.ru>
2022-10-17 20:30:22 +0300

    Add Example010 IntroArray

--- Example010_IntroArray/Program.cs
+++ Example010_IntroArray/Program.cs
@@ -6,12 +6,10 @@
     return result; 
 } //           0   1  2  3 ...         8
 int[] array = {11,21,31,41,15,61,17,18,19}; //массив 
-array[0] = 12;
-Console.WriteLine(array[4]);
 
-//int max = Max (
-   // Max(a1, b1, c1),
-   // Max(a2, b2, c2),
-    //Max(a3, b3, c3));
+int max = Max (
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
 
-//Console.WriteLine(max);
+Console.WriteLine(max);
