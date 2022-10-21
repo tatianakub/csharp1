fbeefb4cc3339e4b614ab76e3c9ffe68a99affd3
tatianakub <pershik86@mail.ru>
2022-10-21 22:47:13 +0300

    Add Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу , которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (х и у)
+
+Console.WriteLine("Введите номер четверти плоскости ");
+string quarter = Console.ReadLine();
+
+string Quarter(string quarterXY)
+{
+    if(quarterXY == "1") return "x > 0 и y > 0";
+    if(quarterXY == "2") return "x < 0 и y > 0";
+    if(quarterXY == "3") return "x < 0 и y < 0";
+    if(quarterXY == "4") return "x > 0 и y < 0";
+    return "Некорректный ввод";
+}
+
+ string xy = Quarter(quarter);
+Console.Write($"Диапазон координат для четверти {quarter}: "); 
+Console.WriteLine(xy);
