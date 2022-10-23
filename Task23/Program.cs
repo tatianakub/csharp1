1f419d7d624b843fb1eda09257e5176906fe4b99
tatianakub <pershik86@mail.ru>
2022-10-23 21:28:56 +0300

    Add Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+while (count <= number)
+{
+    int result = count*count*count;
+    Console.WriteLine($"{count} | {result}");
+    count++;
+}
