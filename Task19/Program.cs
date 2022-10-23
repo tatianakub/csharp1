f4cc6a97b1084c31ec7cc6e2f5a5c0ac4a9de5ae
tatianakub <pershik86@mail.ru>
2022-10-23 21:15:44 +0300

    Add Task21

---
+++ Task19/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.WriteLine("Введите целое пятизначное число ");
+string number = Convert.ToString(Console.ReadLine()); 
+
+//char[] arr = number.ToString().ToCharArray(); //задали массив
+
+static bool IsPalindrome(string number)
+{
+    for (int i = 0; i < number.Length / 2; ++i)
+        if (number[i] != number[number.Length - 1 - i]) return false;
+    return true;
+}
+
+if (IsPalindrome(number) = true)
+{
+    Console.WriteLine("да");
+}
+else Console.WriteLine("нет");
---
+++ Task21/Program.cs
@@ -0,0 +1,39 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.Write("Введите координату точки х1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Введите координату точки y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату точки z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Введите координату точки х2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Введите координату точки y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату точки z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double Range(int x11, int y11, int z11, int x21, int y21, int z21)
+{
+    int rangeX = x11 - x21;
+    int rangeY = y11 - y21;
+    int rangeZ = z11 - z21;
+    double rangeA = Math.Sqrt(rangeX*rangeX + rangeY*rangeY + rangeZ*rangeZ);
+    return rangeA;
+}
+double result = Range(x1, y1, z1, x2, y2, z2);
+
+Console.Write($"А ({x1},{y1},{z1}); B ({x2},{y2},{z2}) -> {Math.Round(result, 2, MidpointRounding.ToZero)}");
