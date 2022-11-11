ebd91fa930b945b62923357d15d3080ed18fd319
tatianakub <pershik86@mail.ru>
2022-11-11 18:56:14 +0300

    Add Task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -3,3 +3,16 @@
 //3, 5 -> 243 (3⁵)
 
 //2, 4 -> 16
+
+Console.WriteLine("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int stepen = Convert.ToInt32(Console.ReadLine());
+
+Math.Pow(x,stepen);
+
+double res = Math.Pow(x,stepen);
+Convert.ToInt32(Math.Pow(x,stepen));
+
+Console.WriteLine($"{x} в степени {stepen} = {res}");
