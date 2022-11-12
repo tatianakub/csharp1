b628e3c635e2188003c2f547370569045f22c573
tatianakub <pershik86@mail.ru>
2022-11-12 17:11:43 +0300

    Add Task29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -3,3 +3,31 @@
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
 
+// Console.WriteLine("Введите количество элементов: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+int num = 8;
+int[] NameArray(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(1, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+
+        if (i < array.Length - 1) Console.Write(",");
+
+    }
+    Console.Write("]");
+}
+int[] nameArray = NameArray(num);
+PrintArray(nameArray);
